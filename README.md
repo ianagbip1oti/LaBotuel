e250213e1ddc00360a04916cc77e559a7c2d8811
ianagbip1oti <ianagbip1oti@gmail.com>
2018-02-25 21:41:43 +0000

    First proof of concept

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # LaBotuel
+
+Bot developed as example and test of concepts in
+[ErisCasper.java](https://github.com/ianagbip1oti/ErisCasper.java).
+
+As ErisCasper.java is not yet published on maven you will need to clone and
+`mvn install` it first, so that LaBotuel can use it.
+
--- src/main/java/com/github/lana/labotuel/LaBotuel.java
+++ src/main/java/com/github/lana/labotuel/LaBotuel.java
@@ -1,31 +1,20 @@
 package com.github.lana.labotuel;
 
-import net.dv8tion.jda.core.AccountType;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
+import com.github.lana.eriscasper.ErisCasper;
 import net.dv8tion.jda.core.MessageBuilder;
-import net.dv8tion.jda.core.events.Event;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.core.hooks.EventListener;
 
-public class LaBotuel implements EventListener {
+public class LaBotuel {
   public static void main(String[] args) throws Exception {
-    JDA jda = new JDABuilder(AccountType.BOT).setToken(System.getenv("BOT_TOKEN")).buildBlocking();
-    jda.addEventListener(new LaBotuel());
-  }
-
-  @Override
-  public void onEvent(Event event) {
-    System.out.println(event);
-    if (event instanceof MessageReceivedEvent) {
-      onMessageReceived((MessageReceivedEvent) event);
-    }
-  }
-
-  private void onMessageReceived(MessageReceivedEvent e) {
-    System.out.println(e.getMessage().getContentRaw());
-    if (e.getMessage().getContentRaw().startsWith("!ping")) {
-      e.getTextChannel().sendMessage(new MessageBuilder().append("pong").build()).queue();
-    }
+    ErisCasper.create()
+        .events()
+        .filter(e -> e instanceof MessageReceivedEvent)
+        .map(e -> (MessageReceivedEvent) e)
+        .filter(e -> e.getMessage().getContentRaw().startsWith("!ping"))
+        .subscribe(
+            e ->
+                e.getTextChannel()
+                    .sendMessage(new MessageBuilder().append("pong").build())
+                    .queue());
   }
 }
